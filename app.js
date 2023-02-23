d06886fb6479c187bfaa7a7f352fce2f587170e7
RedJyu <piotr.stezycki@protonmai.com>
2023-02-23 12:57:14 +0100

    added fixed nav

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const date = document.getElementById('date');
 const navToggle = document.querySelector('.nav-toggle');
 const linksCont = document.querySelector('.links-container');
 const links = document.querySelector('.links');
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link');
 
 navToggle.addEventListener('click', () => {
   const containerHeight = linksCont.getBoundingClientRect().height;
@@ -10,6 +12,16 @@ navToggle.addEventListener('click', () => {
     linksCont.style.height = `${linksHeight}px`;
   } else {
     linksCont.style.height = 0;
+  }
+});
+
+window.addEventListener('scroll', () => {
+  const scroll = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scroll > navHeight) {
+    navbar.classList.add('fixed-nav');
+  } else {
+    navbar.classList.remove('fixed-nav');
   }
 });
 
